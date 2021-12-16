9b2b730a72745ab717a848d2eedbf84a29d8c2b7
Vladimir <vladimir.loginov@intel.com>
2021-12-16 14:10:10 +0300

    fix lab1

--- lab1.c
+++ lab1.c
@@ -5,13 +5,23 @@
 
 int main(int argc, char *argv[]) {
   int i = 0;
+  int state = (isspace(argv[1][0]) > 0);
   int answer = 0;
+  if (!state)
+    answer += 1;
   char cur;
-#pragma omp parallel for
-  for (i = 0; i < strlen(argv[1]); i++) {
-#pragma omp atomic update
-    answer += (isspace(argv[1][i]) > 0);
+  while (i < strlen(argv[1])) {
+    if (isspace(argv[1][i]) && !state) {
+      state = 1;
+    } else if (!isspace(argv[1][i]) && state) {
+      state = 0;
+      answer += 1;
+    }
+    i += 1;
   }
+  // for (i = 0; i < strlen(argv[1]); i++) {
+  //   answer += (isspace(argv[1][i]) > 0);
+  // }
 
   printf("Num words in %s = %d\n", argv[1], answer);
 }
